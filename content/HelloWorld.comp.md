919fa41347c6baae12e4deb2c7b4042eb3faa279
shortnd <ococncol@gmail.com>
2018-08-20 23:51:56 -0400

    got nuxtent setup working

---
+++ content/HelloWorld.comp.md
@@ -0,0 +1,10 @@
+---
+title: Hello World
+---
+
+## H2 heading
+
+I started in the content directory and somehow made it all the way here!
+
+
+@[Showcase]
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="home-container">
+    <div>
+      <h2 class="home-slogan">
+        Seamlessly use content files in your Nuxt.js sites.
+      </h2>
+      <div class="home-links">
+        <nuxt-link to="/hello-world" class="button--green">See your first post</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.home-container {
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.home-slogan {
+  font-weight: 300;
+  font-size:42px;
+  color:#526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.home-links {
+  padding-top: 15px;
+}
+
+</style>
