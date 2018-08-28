f863b6a9b617c3da397916f65eab49f27f2e0625
shortnd <ococncol@gmail.com>
2018-08-28 18:17:09 -0400

    added working nuxtent

--- nuxtent.config.js
+++ nuxtent.config.js
@@ -8,6 +8,36 @@ module.exports = {
     ],
     isPost: false
   },
+  // This is the example for multiple content types
+  /**
+   * content: [
+   *  [
+   *    'site_info', {
+   *      permalink: '/site_info/:slug',
+   *      page: 'site_info/_slug',//Needs to have a page but keep it blank to keep it from rendering
+   *      isPost: false,
+   *      generate: [
+   *         // for static build
+   *        'get',
+   *        'getAll'
+   *      ]
+   *    }
+   *  ],
+   *  [
+   *    'projects',
+   *    {
+   *      permalink: '/projects/:slug',
+   *      page: 'projects/_slug',
+   *      isPost: false,
+   *      generate: [
+   *        // for static build
+   *        'get',
+   *        'getAll'
+   *      ]
+   *    }
+   *  ]
+   * ]
+   */
   api: {
     baseURL: 'http://localhost:3000',
     browserBaseURL: 'http://localhost:3000'
---
+++ store/index.js
@@ -0,0 +1,19 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      //
+    },
+    mutations: {
+      //
+    },
+    actions: {
+      // async nuxtServerInit({commit}, {app}) {
+      // await app.$axios.$get('/site_info').then(res => commit('',res))
+      // }
+    }
+  })
+}
+
+export default createStore
